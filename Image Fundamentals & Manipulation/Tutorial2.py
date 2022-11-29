3d5daec3f9829843aad03e872b01d9d8ae8fb7fc
Sami Munir <smunir2001@gmail.com>
2022-11-29 12:50:38 -0500

    complete Tutorial2

--- Image Fundamentals & Manipulation/Tutorial2.py
+++ Image Fundamentals & Manipulation/Tutorial2.py
@@ -1,3 +1,19 @@
 import cv2
+import random
 
 img = cv2.imread('../assets/MSFS2020_SS_1.png', -1)
+# print(type(img), '\n')
+# print(img.shape, '\n')
+# print(img)
+# print(img[0][45], '\n')
+
+# for i in range(100):
+#     for j in range(img.shape[1]):
+#         img[i][j] = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
+
+tag = img[850:1050, 200:500]
+img[100:300, 650:950] = tag
+
+cv2.imshow('Image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
